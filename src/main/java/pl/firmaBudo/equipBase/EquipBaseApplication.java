69f4752823ca63079600288576bea46b216c97d8
Piotr S <psiemb@tlen.pl>
2020-12-11 23:19:24 +0100

    second commit

---
+++ src/main/java/pl/firmaBudo/equipBase/EquipBaseApplication.java
@@ -0,0 +1,13 @@
+package pl.firmaBudo.equipBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/firmaBudo/equipBase/EquipBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.firmaBudo.equipBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
