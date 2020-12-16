3c9bcbbffe3b43c338bf5d1d9c00e186d876e0ca
Piotr S <psiemb@tlen.pl>
2020-12-16 20:35:12 +0100

    [Task-6] Add getByType method

--- src/main/java/pl/firmaBudo/equipBase/dao/a/ContainerDataBase.java
+++ src/main/java/pl/firmaBudo/equipBase/dao/a/ContainerDataBase.java
@@ -2,12 +2,14 @@ package pl.firmaBudo.equipBase.dao.a;
 
 import org.springframework.stereotype.Component;
 import pl.firmaBudo.equipBase.dao.entity.Container;
+import pl.firmaBudo.equipBase.dao.entity.ContainerType;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class ContainerDataBase {
+
 
     private List<Container> containers = new ArrayList<>();
 
@@ -22,16 +24,20 @@ public class ContainerDataBase {
         return this.containers;
     }
 
-    public void addContainer(Container container) {
-        this.containers.add(container);
+    public Container getByType(ContainerType type) {
+        return containers.stream()
+                .filter(container -> container.getType() == type)
+                .findFirst()
+                .orElse(null);
     }
 
-    public Container deleteById(long id) {
-        return containers.stream()
-                .filter(container -> container.getId() == id)
-                .reduce()
-                //.findFirst()
-                .orElse(null);
+    public Container addContainer(Container container) {
+        this.containers.add(container);
+        return container;
+    }
+
+    public void deleteById(long id) {
+        containers.removeIf(container -> container.getId() == id);
     }
 
 }
