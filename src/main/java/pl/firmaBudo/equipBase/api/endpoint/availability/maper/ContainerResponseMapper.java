258b0e45022d3d423476d71cf3acf34661a3c968
Piotr S <psiemb@tlen.pl>
2021-01-06 14:18:11 +0100

    [Task_dailyCost_&_byMaxCost] Add field dailyCost to ContainerResponseMapper.mapToCheckContainer

--- src/main/java/pl/firmaBudo/equipBase/api/endpoint/availability/maper/ContainerResponseMapper.java
+++ src/main/java/pl/firmaBudo/equipBase/api/endpoint/availability/maper/ContainerResponseMapper.java
@@ -2,7 +2,7 @@ package pl.firmaBudo.equipBase.api.endpoint.availability.maper;
 
 import org.springframework.stereotype.Component;
 import pl.firmaBudo.equipBase.api.endpoint.availability.response.AvailabilityContainer;
-import pl.firmaBudo.equipBase.api.endpoint.availability.response.CheckContainerByTypeResponse;
+import pl.firmaBudo.equipBase.api.endpoint.availability.response.CheckContainerResponse;
 import pl.firmaBudo.equipBase.api.endpoint.availability.response.ContainerResponse;
 import pl.firmaBudo.equipBase.dao.entity.ContainerEntity;
 import pl.firmaBudo.equipBase.dao.entity.ContainerType;
@@ -31,19 +31,26 @@ public class ContainerResponseMapper {
         return new ContainerResponse(availabilityContainers);
     }
 
-    public List<CheckContainerByTypeResponse> mapToResponseByType(List<ContainerEntity> containerEntities) {
+    public List<CheckContainerResponse> mapToResponseByParameter(List<ContainerEntity> containerEntities) {
         return containerEntities.stream()
                 .map(this::mapToCheckContainer)
                 .collect(Collectors.toList());
     }
 
-    private CheckContainerByTypeResponse mapToCheckContainer(ContainerEntity containerEntity) {
-        CheckContainerByTypeResponse checkContainerByTypeResponse = new CheckContainerByTypeResponse();
-        checkContainerByTypeResponse.setId(containerEntity.getId());
-        checkContainerByTypeResponse.setType(containerEntity.getType());
-        checkContainerByTypeResponse.setWorkerCapacity(containerEntity.getWorkerCapacity());
+    private CheckContainerResponse mapToCheckContainer(ContainerEntity containerEntity) {
+        CheckContainerResponse checkContainerResponse = new CheckContainerResponse();
+        checkContainerResponse.setId(containerEntity.getId());
+        checkContainerResponse.setType(containerEntity.getType());
+        checkContainerResponse.setWorkerCapacity(containerEntity.getWorkerCapacity());
+        checkContainerResponse.setDailyCost(containerEntity.getDailyCost());
 
-        return checkContainerByTypeResponse;
+
+        return checkContainerResponse;
     }
+
+//    public List<CheckContainerResponse> mapToResponseByCost(List<ContainerEntity> containerEntities){
+//        return containerEntities.stream()
+//                .map()
+//    }
 }
  
